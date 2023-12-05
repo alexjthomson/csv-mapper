ff91c0784ba291c5a5812532b5485a9bb2e6e47a
Alex Thomson <alexjthomson1882@gmail.com>
2023-12-05 16:28:14 +0000

    Removed admin app (using Django built-in instead), created dashboard with templates

--- csv_mapper/dashboard/views.py
+++ csv_mapper/dashboard/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+# Dashboard view:
+def dashboard(request):
+    return render(request, 'dashboard.html')
