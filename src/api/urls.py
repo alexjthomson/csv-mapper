c1bb5335fb5782bda0368f98d00acc65eb6acb69
Alex Thomson <alexjthomson1882@gmail.com>
2023-12-07 23:40:03 +0000

    Started adding graphing API

--- src/api/urls.py
+++ src/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('source', views.source, name='source'),
+    path('graph', views.graph, name='graph')
 ]
--- src/api/views.py
+++ src/api/views.py
@@ -1,3 +1,153 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
+from .models import DataSourceModel
+import json
 
-# Create your views here.
+@login_required
+def source(request):
+    """
+    API endpoint for managing CSV sources.
+
+    This endpoint will perform different actions depending on the HTTP request
+    method used to access the endpoint.
+    """
+
+    # Get the json data:
+    try:
+        request_data = json.loads(request.body.decode('utf-8'))
+    except json.JSONDecodeError:
+        response_data = {
+            'result': 'error',
+            'message': 'Invalid JSON request body.'
+        }
+        return JsonResponse(response_data, status=400)
+
+    if request.method == 'GET':
+        """
+        The `GET` method will fetch the data from the data-source. This can be
+        used to construct a graph.
+        """
+
+        # Get the source ID:
+        source_id = request_data.get('source_id')
+        if source_id is None:
+            response_data = {
+                'result': 'error',
+                'message': 'No `source_id` provided.'
+            }
+            return JsonResponse(response_data, status=400)
+        elif not isinstance(source_id, int):
+            response_data = {
+                'result': 'error',
+                'message': 'Invalid `source_id`.'
+            }
+            return JsonResponse(response_data, status=400)
+        
+        # Check if transformations should be applied to the source data:
+        transform = request_data.get('transform')
+        if transform is None:
+            transform = False
+        
+        # Get the source data:
+        try:
+            data_source = DataSourceModel.objects.get(id=source_id)
+        except DataSourceModel.DoesNotExist:
+            response_data = {
+                'result': 'error',
+                'message': f'No source found for ID `{source_id}`.'
+            }
+            return JsonResponse(response_data, status=400)
+
+        # TODO: Get the data at the source here
+        # TODO: Add transformation here
+
+        response_data = {
+            'result': 'success'
+        }
+        return JsonResponse(response_data, status=200)
+    elif request.method == 'PUT':
+        """
+        The `PUT` method will replace all current representations of the target
+        resource with the request payload. Simply put, this will create or
+        update the CSV resource with the request payload.
+        """
+
+        # TODO: Check that the user has permission to perform this action
+
+        # Get and validate the `source_name`:
+        source_name = request_data.get('source_name')
+        if source_name is None:
+            response_data = {
+                'result': 'error',
+                'message': 'No `source_name` provided.'
+            }
+            return JsonResponse(response_data, status=400)
+        elif not isinstance(source_name, str):
+            response_data = {
+                'result': 'error',
+                'message': 'Invalid `source_name`.'
+            }
+            return JsonResponse(response_data, status=400)
+        
+        # Get and validate the `source_location`:
+        source_location = request_data.get('source_location')
+        if source_location is None:
+            response_data = {
+                'result': 'error',
+                'message': 'No `source_location` provided.'
+            }
+            return JsonResponse(response_data, status=400)
+        elif not isinstance(source_location, str):
+            response_data = {
+                'result': 'error',
+                'message': 'Invalid `source_location`.'
+            }
+            return JsonResponse(response_data, status=400)
+
+        # Create the source:
+        source_data = {
+            'name': source_name,
+            'location': source_location
+        }
+        try:
+            source_instance = DataSourceModel.objects.get_or_create(name=source_name, defaults=source_data)
+            source_instance.save()
+        except ValidationError:
+            response_data = {
+                'result': 'error',
+                'message': 'Failed to validate source data.'
+            }
+            return JsonResponse(response_data, status=400)
+        except Exception:
+            response_data = {
+                'result': 'error',
+                'message': 'Failed to update source.'
+            }
+            return JsonResponse(response_data, status=500)
+
+        # Return the success response:
+        response_data = {
+            'result': 'success',
+            'message': 'The source was updated successfully.'
+        }
+        return JsonResponse(response_data, status=200)
+    elif request.method == 'DELETE':
+        """
+        The `DELETE` method will delete the described source.
+        """
+        # TODO
+        response_data = {
+            'result': 'error',
+            'message': 'Not implemented.'
+        }
+        return JsonResponse(response_data, status=500)
+    else:
+        response_data = {
+            'result': 'error',
+            'message': f'Unrecognised HTTP request method: `{request.method}`.'
+        }
+        return JsonResponse(response_data, status=400)
+
+def graph(request):
+    pass
