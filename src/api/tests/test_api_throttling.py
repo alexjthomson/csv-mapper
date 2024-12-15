d3b79bf21fe20903542dc68a830ffb6eb81485c0
Alex Thomson <alexjthomson1882@gmail.com>
2024-12-15 17:49:43 +0000

    Fixed throtting unit test

--- src/api/tests/test_api_throttling.py
+++ src/api/tests/test_api_throttling.py
@@ -1,17 +1,38 @@
 from rest_framework.test import APIClient, APITestCase
 from rest_framework import status
 
+from django.contrib.auth.models import User, Group, Permission
+from django.contrib.contenttypes.models import ContentType
+from django.test import RequestFactory
 from rest_framework.test import APIClient, APITestCase
-from rest_framework import status
-from django.contrib.auth.models import User
+from unittest.mock import patch, MagicMock
+from api.models import Source
+from api.views.source import *
+from io import StringIO
 
 class ThrottlingTestCase(APITestCase):
     def setUp(self):
-        self.client = APIClient()
-        self.url = '/api/source/'
+        self.factory = RequestFactory()
 
-        # Create a test user
+        # Create the required 'default' group
+        self.default_group = Group.objects.create(name='default')
+
+        # Create test users
         self.user = User.objects.create_user(username='testuser', password='password')
+
+        # Assign permissions to the user with permissions
+        permissions = [
+            'view_source',
+            'add_source',
+        ]
+        content_type = ContentType.objects.get(app_label='api', model='source')
+        for codename in permissions:
+            permission = Permission.objects.get(codename=codename, content_type=content_type)
+            self.user.user_permissions.add(permission)
+
+        # APIClient setup
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
 
     def test_anonymous_throttling(self):
         throttle_limit = 500 # Anonymous rate limit
@@ -19,7 +40,7 @@ class ThrottlingTestCase(APITestCase):
         # Make requests as an anonymous user
         for _ in range(throttle_limit):
             response = self.client.get(self.url)
-            self.assertEqual(response.status_code, status.HTTP_200_OK)
+            self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
         # Exceed the throttle limit
         response = self.client.get(self.url)
